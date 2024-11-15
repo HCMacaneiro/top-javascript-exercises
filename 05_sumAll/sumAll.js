215d746b1a53bf8636c7a4e65bbb6ae91fc59bfa
Henrique <87825553+HCMacaneiro@users.noreply.github.com>
2024-11-15 15:46:10 -0300

    Completes exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+    if (Number.isInteger(startInt) && Number.isInteger(endInt) && startInt > 0 && endInt > 0) {
+        let result = 0;
+        let largerInt;
+        let smallerInt;
+        if (startInt < endInt) {
+            largerInt = endInt;
+            smallerInt = startInt;
+        }
+        else {
+            largerInt = startInt;
+            smallerInt = endInt;
+        }
+        for (smallerInt; smallerInt <= largerInt; smallerInt++) {
+            result += smallerInt;
+        }
+        return result;
+    }
+    else {
+        return "ERROR";
+    }
 
 };
 
