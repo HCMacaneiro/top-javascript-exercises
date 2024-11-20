0df5fdd0606c0d5c5c1461a113b897f8992b3094
Henrique <87825553+HCMacaneiro@users.noreply.github.com>
2024-11-20 20:54:23 -0300

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    arr.sort((a, b) => {
+        if ("yearOfDeath" in a && !("yearOfDeath" in b)) {
+            const currentYear = new Date().getFullYear();
+            const oldest = a.yearOfBirth - a.yearOfDeath;
+            const youngest = b.yearOfBirth - currentYear;
+            return oldest - youngest;
+        }
+        else if ("yearOfDeath" in b && !("yearOfDeath" in a)) {
+            const currentYear = new Date().getFullYear();
+            const oldest = a.yearOfBirth - currentYear;
+            const youngest = b.yearOfBirth - b.yearOfDeath;
+            return oldest - youngest;
+        }
+        const oldest = a.yearOfBirth - a.yearOfDeath;
+        const youngest = b.yearOfBirth - b.yearOfDeath;
+        return oldest - youngest;
+    })
 
+    return arr[0];
 };
 
 // Do not edit below this line
