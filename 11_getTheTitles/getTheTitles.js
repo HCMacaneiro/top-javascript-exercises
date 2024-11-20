059604e6f1c8b4a22ef3324964dc32acd5dd4ec0
Henrique <87825553+HCMacaneiro@users.noreply.github.com>
2024-11-20 20:26:52 -0300

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    const titles = [];
+    for (obj of arr) {
+        titles.push(obj.title);
+    }
+    return titles;
 
 };
 
