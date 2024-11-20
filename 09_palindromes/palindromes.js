2105d06675f068b4e72dbb3f8676639564fbe4c4
Henrique <87825553+HCMacaneiro@users.noreply.github.com>
2024-11-20 20:06:31 -0300

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const wordArr = word.split('');
+    const newWordArr = wordArr.filter(letter => { 
+        if(
+            letter !== " " &&
+            letter !== "!" &&
+            letter !== "," &&
+            letter !== "."
+        )
+            return true;
+    })
+    const newWord = newWordArr.join('');
+    const newWordReverse = newWordArr.reverse().join('');
+    return newWord.toLowerCase() == newWordReverse.toLowerCase();
 };
 
 // Do not edit below this line
